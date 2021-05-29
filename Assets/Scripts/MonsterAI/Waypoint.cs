1ce43587656a02112e3404e8f6fc46e3b2b5ce49
Tim Ogden <togden1999@gmail.com>
2021-05-28 22:51:37 -0400

    Add connect and disconnect nodes button

--- Assets/Scripts/MonsterAI/Waypoint.cs
+++ Assets/Scripts/MonsterAI/Waypoint.cs
@@ -12,6 +12,8 @@ public class Waypoint : MonoBehaviour
     public List<Waypoint> neighbors = new List<Waypoint>();
     public bool oneWay;
     public bool createNeighbor;
+    public bool connect;
+    public bool disconnect;
     public Waypoint previous
     	{ get; set; }
     public float distance
@@ -56,6 +58,10 @@ public class Waypoint : MonoBehaviour
 	    }
 
         if(createNeighbor) {
+            if(Selection.objects.Length>1) {
+                Debug.Log("Too many waypoints selected, adjacent nodes would be ambiguous.");
+                return;
+            }
             createNeighbor = false;
             Waypoint newPoint = Object.Instantiate(waypointPrefab, transform.parent).GetComponent<Waypoint>();
             newPoint.neighbors = new List<Waypoint>();
@@ -63,6 +69,37 @@ public class Waypoint : MonoBehaviour
             if(!oneWay)
                 neighbors.Add(newPoint);
             Selection.activeGameObject = newPoint.gameObject;
+        }
+
+        if(connect) {
+            connect = false;
+            if(Selection.objects.Length<2) {
+                Debug.Log("Not enough waypoints selected!");
+                return;
+            }
+            foreach(GameObject waypoint in Selection.objects) {
+                if(waypoint.TryGetComponent(out Waypoint way)) {
+                    if(way==this)
+                        continue;
+                    neighbors.Add(way);
+                }
+            }
+        }
+
+        if(disconnect) {
+            disconnect = false;
+            if(Selection.objects.Length<2) {
+                Debug.Log("Not enough waypoints selected!");
+                return;
+            }
+            foreach(GameObject a in Selection.objects) {
+                foreach(GameObject b in Selection.objects) {
+                    if(a.TryGetComponent(out Waypoint waypoint_a) && b.TryGetComponent(out Waypoint waypoint_b)) {
+                        if(waypoint_a.neighbors.Contains(waypoint_b))
+                            waypoint_a.neighbors.Remove(waypoint_b);
+                    }
+                }
+            }
         }
     }
 
